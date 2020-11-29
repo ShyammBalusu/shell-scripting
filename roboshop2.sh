2e638f2a227d5916e326dce3807673abad327a58
ShyammBalusu <saibalusu1992@gmail.com>
2020-11-29 23:39:04 +0530

    Pushing to the my git hub

---
+++ roboshop2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+case $1 in
+     frontend)
+  echo installing frontend
+  echo completed installing frontend
+  ;;
+     catalouge)
+  echo installing catalouge
+  echo completed installing catalouge
+  ;;
+     cart)
+  echo installing cart
+  echo completed installing cart
+  ;;
+esac
+
+
+
+
